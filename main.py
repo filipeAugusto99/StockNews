974a51ccf1c434e0404930be8ed82d834ab7f2cd
filipeAugusto99 <filipecordeiro213@gmail.com>
2026-02-22 15:17:03 -0300

    git 01

---
+++ main.py
@@ -0,0 +1,70 @@
+import requests
+from datetime import datetime
+
+
+INTERVAL_DAY = "1day"
+STOCK = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+FIVE_PERCENT = 0.05
+
+
+STOCK_ENDPOINT = "https://api.twelvedata.com/time_series"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+API_KEY_NEWS = "108913b04241447aacaa67b648a9b554"
+API_KEY_STOCK = "685aea4540fc4cdea9221b0f5f9bfdd6"
+
+
+## STEP 1: Use https://newsapi.org/docs/endpoints/everything
+# When STOCK price increase/decreases by 5% between y esterday andthe day before yesterday then print("Get News").
+    #HINT 1: Get the closing price for yesterday and the day before yesterday. Find the positive difference between the two prices. e.g. 40 - 20 = -20, but the positive difference is 20.
+stock_params = {
+    "symbol": STOCK,
+    "interval": INTERVAL_DAY,
+    "apikey": API_KEY_STOCK,
+}
+#
+# date_now = datetime.now()
+# month = date_now.month
+# day = date_now.day
+# day_moment = f"0{month}-{day}"
+# print(day_moment)
+
+stock_response = requests.get(STOCK_ENDPOINT, params=stock_params)
+stock_response.raise_for_status()
+
+stock_data = stock_response.json()
+yesterday_close = float(stock_data["values"][0]['close'])
+before_yesterday = float(stock_data["values"][1]['close'])
+difference = abs(yesterday_close - before_yesterday)
+
+#     #HINT 2: Work out the value of 5% of yerstday's closing stock price.
+percent = (difference / yesterday_close) * 100
+print(percent)
+if percent > FIVE_PERCENT:
+    print("GET NEWS!")
+
+
+## STEP 2: Use https://newsapi.org/docs/endpoints/everything
+# Instead of printing ("Get News"), actually fetch the first 3 articles for the COMPANY_NAME. 
+#HINT 1: Think about using the Python Slice Operator
+
+
+
+## STEP 3: Use twilio.com/docs/sms/quickstart/python
+# Send a separate message with each article's title and description to your phone number. 
+#HINT 1: Consider using a List Comprehension.
+
+
+
+#Optional: Format the SMS message like this: 
+"""
+TSLA: 🔺2%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+or
+"TSLA: 🔻5%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+"""
+
